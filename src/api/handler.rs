541bb3e637f9da3e067dfb92baf32ec5e2a76df4
wudi <wuuudiii@qq.com>
2023-03-08 19:32:23 +0800

    feat: 实现了get_stream，能够开启新线程执行分片操作，目前可以实现播放
    1. 考虑在转码场景中，将不同的转码方案（根据codec）分为不同的函数。不同的函数套用不同的transcode.toml的模板，这个是第一个，可以作为默认。

---
+++ src/api/handler.rs
@@ -0,0 +1,52 @@
+use hyper::{header, Body, Response, StatusCode};
+use crate::config::{Config, self};
+use crate::ffmpeg::FFmpeg;
+use std::sync::Arc;
+
+macro_rules! json {
+    ($x:expr) => {
+        match serde_json::to_string($x) {
+            Ok(json) => Response::builder()
+            .header(header::CONTENT_TYPE, "application/json")
+                .header("Access-Control-Allow-Origin", "*")
+                .body(Body::from(json))
+                .unwrap(),
+            Err(_) => Response::builder()
+                .header("Access-Control-Allow-Origin", "*")
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body("INTERNAL_SERVER_ERROR".into())
+                .unwrap(),
+        }
+    };
+}
+
+pub async fn get_stream(
+    // db目前没有作用先删除
+    // db: SharedDb,
+    config: Arc<Config>,
+    id: i32,
+) -> Result<Response<Body>, hyper::Error> {
+    let config = Arc::new(config.ffmpeg.clone());
+
+    // 创建ffmpeg，使用相应config进行初始化
+    let ffmpeg = FFmpeg::new(config);
+
+    // Runtime::channel()创建带有关联的发送者。当想要从另一个线程流式传输chunk时很有用。
+    let (tx, body) = Body::channel();
+
+    // 开启新的线程调用ffmpeg下的transcode方法
+    tokio::spawn(async move {
+        ffmpeg.transcode("h264.mkv", tx).await;
+    });
+
+    // 构建给前端的返回值，返回值包含了body，body在主线程中接收来自新线程转码后的结果
+    let resp = Response::builder()
+    .header("Content-Type", "video/mp4")
+    .header("Content-Disposition", "inline")
+    .header("Content-Transfer-Enconding", "binary")
+    .body(body)
+    .unwrap();
+
+    Ok(resp)
+}
+
